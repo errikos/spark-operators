d777704f66d5eaf5e4a3f0cad4b0bef001d75371
Ergys Dona <errikosd@gmail.com>
2018-05-11 05:07:12 +0200

    cube: cleanup Main
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

--- spark-operators/src/main/scala/cubeoperator/Main.scala
+++ spark-operators/src/main/scala/cubeoperator/Main.scala
@@ -3,21 +3,19 @@ package cubeoperator
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
-
 object CubeRunner {
 
   val numReducers = 10
 
   def apply(inputPath: String): RDD[(String, Double)] = {
-    // setup Spark session and context
+    // setup Spark context
     val ctx = SparkSession.builder
       .master("local[16]")
       .appName("CS422-Project2")
       .getOrCreate()
-    val sqlContext = ctx.sqlContext
 
     // load input file into a DataFrame
-    val df = sqlContext.read
+    val df = ctx.sqlContext.read
       .format("com.databricks.spark.csv")
       .option("header", "true")
       .option("inferSchema", "true")
@@ -25,19 +23,20 @@ object CubeRunner {
       .load(inputPath)
 
     // create Dataset
-    val dataset = Dataset(df.rdd, df.schema map { _.name } toList)
+    val dataset = Dataset(df.rdd, df.schema.map { _.name }.toList)
 
     // create Cube operator and run
     val cb = CubeOperator(numReducers)
-    var groupingList = List("lo_suppkey", "lo_shipmode", "lo_orderdate")
+    val groupingList = List("lo_suppkey", "lo_shipmode", "lo_orderdate")
     cb.cube(dataset, groupingList, "lo_supplycost", "SUM")
 
     /**
-      * The above call corresponds to the query:
-      * SELECT lo_suppkey, lo_shipmode, lo_orderdate, SUM (lo_supplycost)
-      * FROM LINEORDER
-      * CUBE BY lo_suppkey, lo_shipmode, lo_orderdate
-      */
+    * The above call corresponds to the query:
+    *
+    * SELECT lo_suppkey, lo_shipmode, lo_orderdate, SUM (lo_supplycost)
+    * FROM LINEORDER
+    * CUBE BY lo_suppkey, lo_shipmode, lo_orderdate
+    */
   }
 }
 
