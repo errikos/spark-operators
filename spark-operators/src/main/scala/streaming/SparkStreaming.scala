9a255c7c8181319dc46aa7b3e0d1920dcfb00b7b
Ergys Dona <errikosd@gmail.com>
2018-05-16 01:57:19 +0200

    task3: implement exact top-k
    Signed-off-by: Ergys Dona <errikosd@gmail.com>

--- spark-operators/src/main/scala/streaming/SparkStreaming.scala
+++ spark-operators/src/main/scala/streaming/SparkStreaming.scala
@@ -3,33 +3,41 @@ package streaming
 import org.apache.spark._
 import org.apache.spark.streaming._
 
-class SparkStreaming(val sparkConf: SparkConf, val args: Array[String]) {
-
+class Env(args: Array[String]) extends Serializable {
+  val checkpointPath = "hdfs://" // TODO
   val inputDirectory = args(0) // the directory in which the stream is expected
   val seconds: Int = args(1).toInt // seconds per window
-  val topK: Int = args(2).toInt // number of heavy hitters stored
+  val topK: Int = args(2).toInt // track the k first hitters; only relevant for "precise" strategy
   val strategy: String = args(3).toLowerCase // "precise" or "approx"
+}
+
+object Env {
+  def apply(args: Array[String]) = new Env(args)
+}
+
+/**
+  * This class acts as the streaming initiator, as well as the environment
+  * to the StreamOperator class hierarchy.
+  *
+  * Streaming operation is triggered with a call to consume().
+  */
+class SparkStreaming(val sparkConf: SparkConf, val args: Array[String]) {
+
+  val env = Env(args)
 
   // create a StreamingContext, the main entry point for all streaming functionality
-  val ssc = new StreamingContext(sparkConf, Seconds(seconds))
+  private def createContext: StreamingContext = {
+    // get the appropriate streaming operator (calls StreamOperator.apply)
+    val streamOperator = StreamOperator(env.strategy)
+    // setup the streaming operations and return the context
+    streamOperator.setup(sparkConf, env)
+  }
 
   def consume() {
-    // create a DStream that represents streaming data from a directory source.
-    val linesDStream = ssc.textFileStream(inputDirectory)
-
-    // parse the stream. (line -> (IP1, IP2))
-    val words = linesDStream.map { x =>
-      (x.split("\t")(0), x.split("\t")(1))
-    }
-
-    // get the appropriate streaming operator (calls StreamOperator.apply)
-    val streamOperator = StreamOperator(strategy, words)
-    // setup the streaming operations
-    streamOperator.setup(this)
-
+    // get or create a new streaming context
+    val ssc = StreamingContext.getOrCreate(env.checkpointPath, createContext _)
     // start the computation
     ssc.start()
-
     // wait for the computation to terminate
     ssc.awaitTermination()
   }
--- spark-operators/src/main/scala/streaming/StreamOperator.scala
+++ spark-operators/src/main/scala/streaming/StreamOperator.scala
@@ -1,24 +1,83 @@
 package streaming
 
-import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-sealed abstract class StreamOperator extends Product with Serializable {
-  def setup(ctx: SparkStreaming)
+import scala.reflect.ClassTag
+
+/**
+  * Base class for Streaming operators.
+  *
+  * Defines a simple interface for setting up and returning a Spark streaming context,
+  * via the setup function, which takes a SparkStreaming environment (defined in
+  * SparkStreaming.scala) and returns a Spark StreamingContext object.
+  *
+  * @author Ergys Dona <ergys.dona@epfl.ch>
+  */
+sealed abstract class StreamOperator extends Serializable {
+
+  /**
+    * This function shall return a new, ready to be started, Spark StreamingContext object.
+    * Only 'lazy' operations shall be performed, i.e. this function shall return immediately.
+    */
+  def setup(conf: SparkConf, env: Env): StreamingContext
 }
 
 object StreamOperator {
-  def apply(strategy: String, stream: DStream[(String, String)]): StreamOperator =
+  def apply(strategy: String): StreamOperator =
     strategy match {
-      case "precise" => PreciseStreamOperator(stream)
-      case "approx"  => ApproximateStreamOperator(stream)
+      case "precise" => PreciseStreamOperator
+      case "approx"  => ApproximateStreamOperator
       case _         => throw UnknownStrategyException(s"Unknown strategy: $strategy")
     }
 }
 
-case class PreciseStreamOperator(stream: DStream[(String, String)]) extends StreamOperator {
-  override def setup(ctx: SparkStreaming): Unit = ???
+object PreciseStreamOperator extends StreamOperator {
+
+  def stateUpdater(vs: Seq[Int], running: Option[Int]): Option[Int] =
+    Some(running.getOrElse(0) + vs.sum)
+
+  override def setup(conf: SparkConf, env: Env): StreamingContext = {
+    // create a new streaming context object
+    val ssc = new StreamingContext(conf, Seconds(env.seconds))
+    // create a DStream that represents streaming data from a directory source.
+    val linesDStream = ssc.textFileStream(env.inputDirectory)
+    // parse the stream and map each line to an IP pair
+    val thisBatchIPs = linesDStream.map { x =>
+      (x.split("\t")(0), x.split("\t")(1))
+    }
+
+    // compute the IP pair hits for the current batch
+    val thisBatchHits = thisBatchIPs
+      .map { (_, 1) }
+      .reduceByKey { _ + _ }
+    // find and print the top K hitters for this batch
+    thisBatchHits.foreachRDD { rdd =>
+      val top = rdd
+        .map { _.swap }
+        .top(env.topK)
+      println(s"This batch: [${top.mkString(",")}]")
+    }
+
+    // compute the IP pair hits globally
+    val globalHits = thisBatchHits
+      .updateStateByKey(stateUpdater)
+      .checkpoint(Seconds(env.seconds << 3))
+    // find and print the top K global hitters
+    globalHits.foreachRDD { rdd =>
+      val top = rdd
+        .map { _.swap }
+        .top(env.topK)
+      println(s"This batch: [${top.mkString(",")}]")
+    }
+
+    // set checkpoint path and return
+    ssc.checkpoint(env.checkpointPath)
+    ssc
+  }
 }
 
-case class ApproximateStreamOperator(stream: DStream[(String, String)]) extends StreamOperator {
-  override def setup(ctx: SparkStreaming): Unit = ???
+object ApproximateStreamOperator extends StreamOperator {
+  override def setup(conf: SparkConf, env: Env): StreamingContext = ???
 }
